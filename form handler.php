75b41a5d2b5a5176ce57ffb7a0612f8d73233c05
comjerry55 <comjerry55@gmail.com>
2022-10-07 21:59:27 +0300

    first commit

---
+++ form handler.php
@@ -0,0 +1,26 @@
+<?php
+$name = $_POST['Name'];
+$visitor_email = $_POST['Email'];
+$subject = $_POST['Subject'];
+$message= $_POST['Message'];
+
+$email_from = 'jss@gmail.com';
+
+$email_subject = 'New Form Submission';
+
+$email_body = "User Name: $name.\n".
+                "User Email: $visitor_email.\n".
+                 "Subject: $subject.\n" .
+                  "User Message: $message.\n".;
+
+$to = 'jss@gmail.com';
+
+$headers = "From: $email_from \r\n";
+
+$headers ="Reply-To: $visitor_email \r\n";
+
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location:contact.html");
+?>
